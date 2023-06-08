6cd11db4924f9f85219848244ff92881267d5d51
codewizo <ongliberius@gmail.com>
2023-06-08 10:31:10 +0300

    Game javscript code

---
+++ game.js
@@ -0,0 +1,34 @@
+
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+        return 'Rock';
+        case 1:
+        return 'Paper';
+        case 2:
+        return 'Scissors';
+    }
+}
+/*function playerSelection() {
+    playerSelection = window.prompt("Rock, Paper or Scissors?");
+}*/
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowercase();
+    computerSelection.toLowercase();
+    if (playerSelection==computerSelection){
+        return "That's a tie! Let's continue."
+    }
+    else if(computerSelection=="rock"){
+        return (playerSelection == "paper") ? "You Win!" : "You lose!"
+    }
+    else if(computerSelection=="paper"){
+        return (playerSelection == "scissors") ? "You Win!" : "You lose!"
+    }
+    else if(computerSelection=="scissors"){
+        return (playerSelection == "rock") ? "You Win!" : "You lose!"
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
